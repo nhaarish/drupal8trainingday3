0fee75cccf3985144d7e203d8a1a9b8597f54dbc
nhaarish <nhaarish@rediffmail.com>
2019-01-23 11:09:17 +0530

    Dynamic routes

--- modules/custom/routing_demo/src/Controller/RouteController.php
+++ modules/custom/routing_demo/src/Controller/RouteController.php
@@ -5,8 +5,21 @@ namespace Drupal\routing_demo\Controller;
 class RouteController {
   public function helloWorld(){
     return[
-      '#type' => 'markup',
+      '#type' => '#markup',
       '#markup' => 'Hello World!'
     ];
   }
+
+  public function helloDynamic($name) {
+    return [
+      '#type' => '#markup',
+      '#markup' => 'Hello ' . $name . '!',
+    ];
+  }
+
+  public function helloDynamicTitleCallback($name) {
+    return 'Hello ' . $name ;
+  }
+
 }
+
